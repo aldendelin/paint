c6fec59974200a3506985cfd4657d94aaec3e864
aldendelin <64019538+aldendelin@users.noreply.github.com>
2020-06-15 19:40:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var database,position,line;
 
 function setup(){
-    createCanvas(500,500);
+   createCanvas(500,500);
    database = firebase.database();
    var spriteposition = database.ref('line/position');
    spriteposition.on('value',readPosition,showError);
@@ -14,22 +14,26 @@ function draw(){
 }
 
 function mouseDragged(){
-    line = createSprite(110,110,10,10);
-    writePosition();
+    line = createSprite(mouseX,mouseY,10,10);
+    writePosition(mouseX,mouseY);
 }
 
 function mouseReleased(){
 
 }
 
-function writePosition(){
-    database.ref('ball/position');
-}
-
 function readPosition(data){
     position = data.val();
     line.x = position.x;
     line.y = position.y;
+}
+
+function writePosition(x,y){
+    database.ref('line/position').set({
+        'x' : position.x,
+        'y' : position.y
+    })
+   
 }
 
 function showError(){
